bf3d4e1ba675d8544cd13db957df2ebaa77566bb
mamo <kodebukun04@gmail.com>
2021-03-03 17:18:53 +0900

    add work department to seed-fu

---
+++ db/fixtures/production/02_work.rb
@@ -0,0 +1,69 @@
+Work.seed do |s|
+  s.id = 1
+  s.name = "勉強会"
+end
+
+Work.seed do |s|
+  s.id = 2
+  s.name = "会計"
+end
+
+Work.seed do |s|
+  s.id = 3
+  s.name = "お祭り幹事"
+end
+
+Work.seed do |s|
+  s.id = 4
+  s.name = "新人担当"
+end
+
+Work.seed do |s|
+  s.id = 5
+  s.name = "整備図書"
+end
+
+Work.seed do |s|
+  s.id = 6
+  s.name = "医療安全"
+end
+
+Work.seed do |s|
+  s.id = 7
+  s.name = "医療情報"
+end
+
+Work.seed do |s|
+  s.id = 8
+  s.name = "情報セキュリティー"
+end
+
+Work.seed do |s|
+  s.id = 9
+  s.name = "キャンパスネット"
+end
+
+Work.seed do |s|
+  s.id = 10
+  s.name = "感染担当"
+end
+
+Work.seed do |s|
+  s.id = 11
+  s.name = "スクラブ"
+end
+
+Work.seed do |s|
+  s.id = 12
+  s.name = "漏洩線量測定"
+end
+
+Work.seed do |s|
+  s.id = 13
+  s.name = "野球"
+end
+
+Work.seed do |s|
+  s.id = 14
+  s.name = "業績集"
+end
