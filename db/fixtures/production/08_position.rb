14be2ae5d8fc108f5af23cafa5a3ef4068c55f13
mamo <kodebukun04@gmail.com>
2021-03-22 16:22:00 +0900

    add seed_fu to production 08_position

---
+++ db/fixtures/production/08_position.rb
@@ -0,0 +1,24 @@
+Position.seed do |s|
+  s.id = 1
+  s.name = "診療放射線技師長"
+end
+
+Position.seed do |s|
+  s.id = 2
+  s.name = "診療放射線技師副技師長"
+end
+
+Position.seed do |s|
+  s.id = 3
+  s.name = "主任診療放射線技師"
+end
+
+Position.seed do |s|
+  s.id = 4
+  s.name = "診療放射線技師"
+end
+
+Position.seed do |s|
+  s.id = 5
+  s.name = "卒業生"
+end
