5b9a0109d3deed0177ccb98649fa7490a1d3a42c
mamo <kodebukun04@gmail.com>
2023-03-30 23:54:27 +0900

    fix_users_miss

--- db/fixtures/production/15_user_add.rb
+++ db/fixtures/production/15_user_add.rb
@@ -1,5 +1,149 @@
 User.seed do |s|
   s.id = 47
+  s.name = "佐藤　凌大"
+  s.login_id = 43103
+  s.password = "ad1176"
+  s.email = "satoryota@med.id.yamagata-u.ac.jp"
+  s.image_name = "default_user.jpg"
+  s.admin = false
+  s.night_shift = true
+  s.day_shift = true
+  s.call = true
+end
+
+User.seed do |s|
+  s.id = 48
+  s.name = "丹　彰大"
+  s.login_id = 43100
+  s.password = "fg1177"
+  s.email = "tanakihiro@med.id.yamagata-u.ac.jp"
+  s.image_name = "default_user.jpg"
+  s.admin = false
+  s.night_shift = true
+  s.day_shift = true
+  s.call = true
+end
+
+User.seed do |s|
+  s.id = 49
+  s.name = "川村　洸"
+  s.login_id = 43101
+  s.password = "jk1188"
+  s.email = "h.kawamura@med.id.yamagata-u.ac.jp"
+  s.image_name = "default_user.jpg"
+  s.admin = false
+  s.night_shift = true
+  s.day_shift = true
+  s.call = true
+end
+
+User.seed do |s|
+  s.id = 50
+  s.name = "齋藤　可鈴"
+  s.login_id = 43102
+  s.password = "zz1199"
+  s.email = "karin.s@med.id.yamagata-u.ac.jp"
+  s.image_name = "default_user.jpg"
+  s.admin = false
+  s.night_shift = true
+  s.day_shift = true
+  s.call = true
+end
+
+User.seed do |s|
+  s.id = 51
+  s.name = "岩崎　龍弥"
+  s.login_id = 43104
+  s.password = "as1200"
+  s.email = "t.iwasaki@med.id.yamagata-u.ac.jp"
+  s.image_name = "default_user.jpg"
+  s.admin = false
+  s.night_shift = true
+  s.day_shift = true
+  s.call = true
+end
+
+User.seed do |s|
+  s.id = 52
+  s.name = "想田　沙紀"
+  s.login_id = 43106
+  s.password = "sh1211"
+  s.email = "sodasaki@med.id.yamagata-u.ac.jp"
+  s.image_name = "default_user.jpg"
+  s.admin = false
+  s.night_shift = false
+  s.day_shift = false
+  s.call = false
+end
+
+User.seed do |s|
+  s.id = 53
+  s.name = "青木　菜々"
+  s.login_id = 43109
+  s.password = "zz1221"
+  s.email = "aokinana@med.id.yamagata-u.ac.jp"
+  s.image_name = "default_user.jpg"
+  s.admin = false
+  s.night_shift = false
+  s.day_shift = false
+  s.call = false
+end
+
+User.seed do |s|
+  s.id = 54
+  s.name = "植木　航大"
+  s.login_id = 43110
+  s.password = "ad1223"
+  s.email = "ueki@med.id.yamagata-u.ac.jp"
+  s.image_name = "default_user.jpg"
+  s.admin = false
+  s.night_shift = false
+  s.day_shift = false
+  s.call = false
+end
+
+User.seed do |s|
+  s.id = 55
+  s.name = "大木　泉水"
+  s.login_id = 43111
+  s.password = "jk1224"
+  s.email = "okiizumi@med.id.yamagata-u.ac.jp"
+  s.image_name = "default_user.jpg"
+  s.admin = false
+  s.night_shift = false
+  s.day_shift = false
+  s.call = false
+end
+
+User.seed do |s|
+  s.id = 56
+  s.name = "佐藤　晴美"
+  s.login_id = 43112
+  s.password = "fg1225"
+  s.email = "harumis62211@gmail.com"
+  s.image_name = "default_user.jpg"
+  s.admin = false
+  s.night_shift = false
+  s.day_shift = false
+  s.call = false
+end
+
+User.seed do |s|
+  s.id = 57
+  s.name = "阿部　利央"
+  s.login_id = 43113
+  s.password = "jk1226"
+  s.email = "aberio@med.id.yamagata-u.ac.jp"
+  s.image_name = "default_user.jpg"
+  s.admin = false
+  s.night_shift = false
+  s.day_shift = false
+  s.call = false
+end
+
+
+User.seed do |s|
+  s.id = 58
   s.name = "天野　緑"
   s.login_id = 70663
   s.password = "gr1227"
@@ -12,7 +156,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 48
+  s.id = 59
   s.name = "斎藤　久美"
   s.login_id = 70447
   s.password = "hk1226"
@@ -25,7 +169,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 49
+  s.id = 60
   s.name = "伊藤　喜久子"
   s.login_id = 70957
   s.password = "bj1228"
@@ -38,7 +182,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 50
+  s.id = 61
   s.name = "中川　千明"
   s.login_id = 70058
   s.password = "mk1229"
@@ -51,7 +195,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 51
+  s.id = 62
   s.name = "遠藤　順子"
   s.login_id = 70080
   s.password = "gr1230"
@@ -64,7 +208,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 52
+  s.id = 63
   s.name = "西久保　怜奈"
   s.login_id = 70285
   s.password = "sg1231"
@@ -77,7 +221,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 53
+  s.id = 64
   s.name = "岡本　千聖"
   s.login_id = 70895
   s.password = "bj1232"
@@ -90,7 +234,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 54
+  s.id = 65
   s.name = "安部　いづみ"
   s.login_id = 70845
   s.password = "hk1233"
@@ -103,7 +247,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 55
+  s.id = 66
   s.name = "伊藤　咲"
   s.login_id = 70571
   s.password = "mk1234"
@@ -116,7 +260,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 56
+  s.id = 67
   s.name = "丸藤　千尋"
   s.login_id = 70874
   s.password = "gr1235"
@@ -129,7 +273,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 57
+  s.id = 68
   s.name = "石澤　喜代子"
   s.login_id = 70008
   s.password = "bj1236"
@@ -142,7 +286,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 58
+  s.id = 69
   s.name = "高橋　良子"
   s.login_id = 71200
   s.password = "hk1237"
@@ -155,7 +299,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 59
+  s.id = 70
   s.name = "石山　五月"
   s.login_id = 77017
   s.password = "mk1238"
@@ -168,7 +312,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 60
+  s.id = 71
   s.name = "安藤　千寿子"
   s.login_id = 77072
   s.password = "gr1239"
@@ -181,7 +325,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 61
+  s.id = 72
   s.name = "想田　光"
   s.login_id = 43094
   s.password = "hk1240"
@@ -194,7 +338,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 62
+  s.id = 73
   s.name = "柴　宏博"
   s.login_id = 43105
   s.password = "bj1242"
@@ -207,7 +351,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 63
+  s.id = 74
   s.name = "李　聖賢"
   s.login_id = 43096
   s.password = "sg1264"
@@ -220,7 +364,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 64
+  s.id = 75
   s.name = "宮坂　友侑也"
   s.login_id = 43081
   s.password = "gr1241"
@@ -233,7 +377,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 65
+  s.id = 76
   s.name = "小野　拓也"
   s.login_id = 43114
   s.password = "sg1265"
@@ -246,7 +390,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 66
+  s.id = 77
   s.name = "桐井　一邦"
   s.login_id = 7504
   s.password = "gr1243"
@@ -259,7 +403,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 67
+  s.id = 78
   s.name = "豊口　祐樹"
   s.login_id = 7510
   s.password = "gr1245"
@@ -272,7 +416,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 68
+  s.id = 79
   s.name = "紺野　義浩"
   s.login_id = 7066
   s.password = "hk1246"
@@ -285,7 +429,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 69
+  s.id = 80
   s.name = "平賀　利匡"
   s.login_id = 7512
   s.password = "mk1247"
@@ -298,7 +442,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 70
+  s.id = 81
   s.name = "菅井　康大"
   s.login_id = 7526
   s.password = "sg1248"
@@ -311,7 +455,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 71
+  s.id = 82
   s.name = "高梨　悠"
   s.login_id = 7537
   s.password = "hk1252"
@@ -324,7 +468,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 72
+  s.id = 83
   s.name = "小林　篤紀"
   s.login_id = 7067
   s.password = "bj1253"
@@ -337,7 +481,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 73
+  s.id = 84
   s.name = "柴田　亜希子"
   s.login_id = 7542
   s.password = "gr1254"
@@ -350,7 +494,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 74
+  s.id = 85
   s.name = "鈴木　啓介"
   s.login_id = 7505
   s.password = "hk1255"
@@ -363,7 +507,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 75
+  s.id = 86
   s.name = "伊勢　元晴"
   s.login_id = 7545
   s.password = "sg1266"
@@ -376,7 +520,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 76
+  s.id = 87
   s.name = "田苗　太陽"
   s.login_id = 20499
   s.password = "sg1267"
@@ -389,7 +533,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 77
+  s.id = 88
   s.name = "市川　真由美"
   s.login_id = 7055
   s.password = "bj1256"
@@ -402,7 +546,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 78
+  s.id = 89
   s.name = "川城　壮平"
   s.login_id = 7514
   s.password = "hk1257"
@@ -415,7 +559,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 79
+  s.id = 90
   s.name = "原田　麻由美"
   s.login_id = 7509
   s.password = "gr1258"
@@ -428,7 +572,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 80
+  s.id = 91
   s.name = "矢野　菜津子"
   s.login_id = 7539
   s.password = "mk1259"
@@ -441,7 +585,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 81
+  s.id = 92
   s.name = "赤松　妃呂子"
   s.login_id = 7511
   s.password = "hk1260"
@@ -454,7 +598,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 82
+  s.id = 93
   s.name = "萩原　靖倫"
   s.login_id = 7068
   s.password = "bj1261"
@@ -467,7 +611,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 83
+  s.id = 94
   s.name = "金子　崇"
   s.login_id = 5724
   s.password = "sg1262"
@@ -480,7 +624,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 84
+  s.id = 95
   s.name = "小野　崇"
   s.login_id = 7518
   s.password = "gr1263"
@@ -493,7 +637,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 85
+  s.id = 96
   s.name = "佐藤　啓"
   s.login_id = 7534
   s.password = "sg1269"
@@ -506,7 +650,7 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 86
+  s.id = 97
   s.name = "大原　紳"
   s.login_id = 7544
   s.password = "pp1271"
@@ -519,10 +663,23 @@ User.seed do |s|
 end
 
 User.seed do |s|
-  s.id = 87
+  s.id = 98
   s.name = "石井　芳樹"
   s.login_id = 7546
   s.password = "pp1272"
+  s.email = ""
+  s.image_name = "default_user.jpg"
+  s.admin = false
+  s.night_shift = false
+  s.day_shift = false
+  s.call = false
+end
+
+User.seed do |s|
+  s.id = 99
+  s.name = "樋口　裕平"
+  s.login_id = 43059
+  s.password = "ag1227"
   s.email = ""
   s.image_name = "default_user.jpg"
   s.admin = false
