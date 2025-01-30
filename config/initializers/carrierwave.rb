1ccffe6a19af65e100fd3ac19161620b741f40c5
mamo <kodebukun04@gmail.com>
2025-01-30 19:53:20 +0900

    fix_add config_initializers_carrierwave.rb

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,16 @@
+if ENV['CLOUDINARY_URL'].present? && Rails.env.production?
+  require 'cloudinary'
+  require 'cloudinary/storage'
+
+  CarrierWave.configure do |config|
+    config.cache_storage = :file # ローカルキャッシュ
+    config.fog_provider = 'fog/cloudinary' # Cloudinaryを利用
+    config.fog_credentials = {
+      provider: 'Cloudinary',
+      cloud_name: ENV['CLOUDINARY_CLOUD_NAME'], # 環境変数から取得
+      api_key: ENV['CLOUDINARY_API_KEY'],
+      api_secret: ENV['CLOUDINARY_API_SECRET']
+    }
+    config.fog_directory = ENV['CLOUDINARY_DIRECTORY'] if ENV['CLOUDINARY_DIRECTORY']
+  end
+end
