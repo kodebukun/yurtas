0e4932732addf5bf0c1ff56916aa78d5405468bf
mamo <kodebukun04@gmail.com>
2025-01-30 19:31:00 +0900

    fix config_initializers_again5

--- config/initializers/carrierwave.rb
+++
@@ -1,6 +0,0 @@
-require 'carrierwave/storage/cloudinary'
-
-CarrierWave.configure do |config|
-  config.storage = :cloudinary
-  config.cloudinary_url = "cloudinary://#{ENV['CLOUDINARY_API_KEY']}:#{ENV['CLOUDINARY_API_SECRET']}@#{ENV['CLOUDINARY_CLOUD_NAME']}"
-end
