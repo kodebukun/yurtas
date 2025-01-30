bbabea304988c69ea493c05793abe0832e4936c8
mamo <kodebukun04@gmail.com>
2025-01-30 17:22:52 +0900

    fix config_initializers_again

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
-CarrierWave.configure do |config|
-  config.cloudinary_credentials = {
-    cloud_name: ENV['CLOUDINARY_CLOUD_NAME'], # CloudinaryのCloud Name
-    api_key: ENV['CLOUDINARY_API_KEY'],      # CloudinaryのAPIキー
-    api_secret: ENV['CLOUDINARY_API_SECRET'] # CloudinaryのAPIシークレット
-  }
+require 'cloudinary'
+require 'carrierwave'
 
-  # ストレージの設定
-  config.cache_storage = :file # 一時ファイルの保存方法
-  config.storage = :cloudinary # Cloudinaryをストレージとして使用
+CarrierWave.configure do |config|
+  config.storage = :cloudinary
+  config.cloudinary = {
+    cloud_name: ENV['CLOUDINARY_CLOUD_NAME'],
+    api_key: ENV['CLOUDINARY_API_KEY'],
+    api_secret: ENV['CLOUDINARY_API_SECRET']
+  }
 end
