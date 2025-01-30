c8e3e99b28021b185988dc5f05be6f5a97065c16
mamo <kodebukun04@gmail.com>
2025-01-30 17:25:43 +0900

    fix config_initializers_again2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,14 @@
 require 'cloudinary'
-require 'carrierwave'
+require 'cloudinary/carrier_wave'
 
 CarrierWave.configure do |config|
-  config.storage = :cloudinary
-  config.cloudinary = {
-    cloud_name: ENV['CLOUDINARY_CLOUD_NAME'],
-    api_key: ENV['CLOUDINARY_API_KEY'],
-    api_secret: ENV['CLOUDINARY_API_SECRET']
-  }
+  config.storage = :file # 開発環境ではローカルストレージを使用
+  if Rails.env.production?
+    config.storage = :cloudinary # 本番環境ではCloudinaryを使用
+    config.cloudinary_config = {
+      cloud_name: ENV['CLOUDINARY_CLOUD_NAME'], # 環境変数から設定
+      api_key: ENV['CLOUDINARY_API_KEY'],
+      api_secret: ENV['CLOUDINARY_API_SECRET']
+    }
+  end
 end
