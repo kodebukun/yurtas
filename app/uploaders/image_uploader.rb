ca8882db8487fd98b59f1d7b3aac4eb99f28159a
mamo <kodebukun04@gmail.com>
2025-02-02 02:49:09 +0900

    fix form

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,13 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
-  storage :cloudinary if Rails.env.production?
   include Cloudinary::CarrierWave
-
-  if Rails.env.production?
-    storage :cloudinary
-  else
-    storage :file
-  end
 
   # 許可するファイル形式
   def extension_allowlist
